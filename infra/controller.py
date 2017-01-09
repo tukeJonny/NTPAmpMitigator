ae16184835779e655020164ab2dd3875d85a9d21
tukejonny <tukejonny@gmail.com>
2017-01-10 01:45:25 +0900

    _add_mitigate_rule update

--- infra/controller.py
+++ infra/controller.py
@@ -11,6 +11,7 @@ from ryu.controller.handler import (
     DEAD_DISPATCHER
 )
 from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ether
 
 class NTPAmpMitigator(simple_switch_13.SimpleSwitch13):
 
@@ -18,13 +19,13 @@ class NTPAmpMitigator(simple_switch_13.SimpleSwitch13):
         super(NTPAmpMitigator, self).__init__(*args, **kwargs)
         self.datapaths = {}
         self.mitigate_match_rule = {
-            'ipv4_src': "10.0.0.0",
-            'nw_src_mask': 8
+            'eth_type': ether.ETH_TYPE_IP,
+            'ipv4_src': ("10.0.0.0", "255.0.0.0"),
         }
 
         self.monitor_thread = hub.spawn(self._monitor)
 
-        self.flag = True
+        #self.flag = True
 
         self.MITIGATE_RULE_EXISTS = False
 
@@ -42,41 +43,43 @@ class NTPAmpMitigator(simple_switch_13.SimpleSwitch13):
     def _monitor(self):
         while True:
             for dp in self.datapaths.values():
-                if self.flag:
-                    self.logger.info("[+] Add Mitigate Rule!")
-                    self._add_mitigate_rule(dp)
-                    self.flag = False
-                else:
-                    self.logger.info("[+] Del Mitigate Rule!")
-                    self._del_mitigate_rule(dp)
-                    self.flag = True
+                # if self.flag:
+                self.logger.info("[+] Add Mitigate Rule!")
+                self._add_mitigate_rule(dp)
+                # self.flag = False
+                # else:
+                #     self.logger.info("[+] Del Mitigate Rule!")
+                #     #self._del_mitigate_rule(dp)
+                #     self.flag = True
+            hub.sleep(10)
+            self.logger.info("[-] Surely, Mitigate rule is expired!")
             hub.sleep(10)
 
-    def _request_stats(self, datapath):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        req = parser.OFPFlowStatsRequest(datapath)
-        datapath.send_msg(req)
-
-        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
-        datapath.send_msg(req)
-
-    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
-    def _flow_stats_reply_handler(self, ev):
-        body = ev.msg.body
-
-        for stat in sorted([flow for flow in body if flow.priority == 1], key=lambda flow: (flow.match['in_port'],flow.match['eth_dst'])):
-            self.logger.info("[+] Flow Stats below")
-            self.logger.info(stat.__dict__)
-
-    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
-    def _port_stats_reply_handler(self, ev):
-        body = ev.msg.body
-
-        for stat in sorted(body, key=attrgetter('port_no')):
-            self.logger.info("[+] Port Stats below")
-            self.logger.info(stat.__dict__)
+    # def _request_stats(self, datapath):
+    #     ofproto = datapath.ofproto
+    #     parser = datapath.ofproto_parser
+    #
+    #     req = parser.OFPFlowStatsRequest(datapath)
+    #     datapath.send_msg(req)
+    #
+    #     req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
+    #     datapath.send_msg(req)
+    #
+    # @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    # def _flow_stats_reply_handler(self, ev):
+    #     body = ev.msg.body
+    #
+    #     for stat in sorted([flow for flow in body if flow.priority == 1], key=lambda flow: (flow.match['in_port'],flow.match['eth_dst'])):
+    #         self.logger.info("[+] Flow Stats below")
+    #         self.logger.info(stat.__dict__)
+    #
+    # @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    # def _port_stats_reply_handler(self, ev):
+    #     body = ev.msg.body
+    #
+    #     for stat in sorted(body, key=attrgetter('port_no')):
+    #         self.logger.info("[+] Port Stats below")
+    #         self.logger.info(stat.__dict__)
 
     def _add_mitigate_rule(self, datapath):
         ofproto = datapath.ofproto
@@ -84,7 +87,16 @@ class NTPAmpMitigator(simple_switch_13.SimpleSwitch13):
 
         match = parser.OFPMatch(**self.mitigate_match_rule)
         actions = []
-        self.add_flow(datapath, 1, match, actions)
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        #self.add_flow(datapath, 20, match, actions) # mitigate rule is high priority
+        msg = parser.OFPFlowMod(
+            datapath=datapath,
+            match=match,
+            priority=20,
+            hard_timeout=10, # Max time before discarding (sec)
+            instructions=inst
+        )
+        datapath.send_msg(msg)
 
     def _del_mitigate_rule(self, datapath):
         ofproto = datapath.ofproto
