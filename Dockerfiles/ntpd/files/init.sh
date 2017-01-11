ee9c5a0979c6b973868383e95c24576b3ce79186
tukejonny <tukejonny@gmail.com>
2017-01-12 00:09:09 +0900

    victim ansible test passed

--- Dockerfiles/ntpd/files/init.sh
+++ Dockerfiles/ntpd/files/init.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# rpm -ivh /tmp/ntp-4.2.6p5-25.el7.centos.x86_64.rpm
-# systemctl enable ntpd
-# systemctl start ntpd
-# firewall-cmd --add-service=ntp --permanent
-# firewall-cmd --reload
+systemctl enable ntpd
+systemctl start ntpd
+firewall-cmd --add-service=ntp --permanent
+firewall-cmd --reload
 
